3e942cc6f3ee38c8af8478129a8dff52ea07b4ac
maxfnine <maxfnine@gmail.com>
2019-05-04 20:40:40 +0300

    Recycler View refactoring

---
+++ app/src/main/java/com/example/coderswag/Adapters/CategoryRecycleAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.coderswag.Adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.coderswag.Adapters.CategoryRecycleAdapter.Holder
+import com.example.coderswag.Model.Category
+import com.example.coderswag.R
+
+class CategoryRecycleAdapter(val context: Context, val categories: List<Category>):RecyclerView.Adapter<Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): Holder {
+
+        val view = LayoutInflater.from(context).inflate(R.layout.category_list_item,parent,false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val category = categories[position]
+        holder.bindCategory(category, context)
+    }
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!){
+        val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
+        val categoryName = itemView?.findViewById<TextView>(R.id.categoryNameText)
+
+        fun bindCategory(category:Category,context: Context){
+
+            categoryName?.text = category.title
+            val resourceId = context.resources.getIdentifier(category.image,"drawable",context.packageName)
+            categoryImage?.setImageResource(resourceId)
+        }
+
+    }
+}
